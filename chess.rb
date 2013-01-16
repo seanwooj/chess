02d481b64b23e726b926f4e9ce03b9b75131c179
Tran and Trautman <unique@kennyletran.com>
2013-01-16 10:19:54 -0800

    Added shells for all classes we will need

---
+++ chess.rb
@@ -0,0 +1,48 @@
+# Here is the game logic
+
+class Game
+end
+
+class Board
+end
+
+# Here are the Players
+class HumanPlayer < Player
+end
+
+class ComputerPlayer < Player
+end
+
+class Player
+end
+
+# Here are the Pieces
+class Piece
+
+end
+
+class Knight < Piece
+
+end
+
+class King < Piece
+
+end
+
+class Queen < Piece
+
+end
+
+class Rook < Piece
+
+end
+
+class Bishop < Piece
+
+end
+
+class Pawn < Piece
+
+end
+
+
